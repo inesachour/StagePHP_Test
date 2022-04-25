7bbd1623b54a2af5ff669ad3657eda8e01a4f248
unknown <inesachour@insat.u-carthage.tn>
2022-04-25 21:01:09 +0200

    api fix problem get

--- Backend/api.php
+++ Backend/api.php
@@ -3,14 +3,11 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-// include database and object files
-
 include './article_controller.php';
-  
-// initialize object
-$product = new ArticleController("GET");
-$product->processRequest();
-  
-// read products will be here
 
+$req_method = $_SERVER['REQUEST_METHOD'];
+  
+$controller = new ArticleController($req_method);
+$controller->processRequest();
+  
 ?>
